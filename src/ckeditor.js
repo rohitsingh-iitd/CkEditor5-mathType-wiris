1aa988637d769c614d2f0f51ec0526324697db4e
Rohit Singh <rohit.singh@alumni.iitd.ac.in>
2020-07-03 23:59:40 +0530

    First commit

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -29,6 +29,7 @@ import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefrom
 import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
 import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation';
+import MathType from '@wiris/mathtype-ckeditor5';
 
 export default class ClassicEditor extends ClassicEditorBase {}
 
@@ -37,6 +38,7 @@ ClassicEditor.builtinPlugins = [
 	Essentials,
 	UploadAdapter,
 	Autoformat,
+	MathType,
 	Bold,
 	Italic,
 	BlockQuote,
@@ -65,6 +67,7 @@ ClassicEditor.defaultConfig = {
 		items: [
 			'heading',
 			'|',
+			'mathType',
 			'bold',
 			'italic',
 			'link',
